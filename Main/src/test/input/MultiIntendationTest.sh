27a8aaf5fdeb9d01783c8c2e354aa9565bcd11a2
ForNeus57 <dominikbreksa@student.agh.edu.pl>
2023-05-09 09:48:19 +0200

    Added input example

--- Main/src/test/input/MultiIntendationTest.sh
+++ Main/src/test/input/MultiIntendationTest.sh
@@ -1,24 +1,23 @@
-```shell
 #!/bin/bash
-while [ $1 == 1 ] ; do
-    case "$1" in 
+while [ "$1" == 1 ] ; do
+    case "$1" in
         asdas )
-            echo "hahahaha" ;
+            echo "Test 1" ;
             if ((1**1 =2 && 1+1=8)) && ((""=""))
             then
+                q="avb"
                 case $q in
                     a)
                         for asd in 1 2 3 4 5 .. 6
                         do
-                        
+                            echo $asd
                         done
-                        
-                    
+
+
                     ;;
                 esac
             fi
         ;;
     esac
 done
-man bash -u | grep "between x and y" | cat 
-```
+man bash -u | grep "between x and y" | cat
