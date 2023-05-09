fc15e3a43dd1c62b7a7475bb634be141ca694470
ForNeus57 <dominikbreksa@student.agh.edu.pl>
2023-05-09 22:25:36 +0200

    Quick fix of previous one

--- Main/src/main/java/pl/edu/agh/kis/Main.java
+++ Main/src/main/java/pl/edu/agh/kis/Main.java
@@ -4,10 +4,10 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import pl.edu.agh.kis.parser.*;
 import pl.edu.agh.kis.parser.BashGrammarLexer;
 import pl.edu.agh.kis.parser.BashGrammarParser;
 
-//! NOTE: when /target/generated-sources/antlr4 files are updated, move them to pl.edu.agh.kis.Parser
 public class Main {
     public static void main(String[] args) {
         //  Lambda performed on each file in the given directory, returns a string written in each output file in Docs/ExampleOutput
--- Main/src/main/java/pl/edu/agh/kis/parser/BashGrammarBaseListener.java
+++ Main/src/main/java/pl/edu/agh/kis/parser/BashGrammarBaseListener.java
@@ -1,4 +1,5 @@
 // Generated from BashGrammar.g4 by ANTLR 4.12.0
+package pl.edu.agh.kis.parser;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
